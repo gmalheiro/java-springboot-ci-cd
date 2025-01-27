103f9e6bf31087b200683babd8c19cec803d3c23
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2025-01-27 14:18:35 -0300

    feat: add welcome endpoint

--- src/main/java/br/com/gmalheiro/springboot_ci_cd/SpringbootCiCdApplication.java
+++ src/main/java/br/com/gmalheiro/springboot_ci_cd/SpringbootCiCdApplication.java
@@ -2,9 +2,17 @@ package br.com.gmalheiro.springboot_ci_cd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringbootCiCdApplication {
+
+	@GetMapping("/welcome")
+	public String welcome () {
+		return  "Welcome";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootCiCdApplication.class, args);
