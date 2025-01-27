10f926e33a0d241c34f15b6f7665dda0a319ed9f
Gabriel Malheiro <gsmalheiro@rd.com.br>
2025-01-27 14:13:26 -0300

    feat: add spring project

---
+++ src/main/java/br/com/gmalheiro/springboot_ci_cd/SpringbootCiCdApplication.java
@@ -0,0 +1,13 @@
+package br.com.gmalheiro.springboot_ci_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gmalheiro/springboot_ci_cd/SpringbootCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gmalheiro.springboot_ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
